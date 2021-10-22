cb52548ae9de8ef4e611db1d306ac4021732d236
truong99 <truongnguyen30999@gmail.com>
2021-10-22 11:44:27 +0700

    fix

--- routes/utilitybills.routes.js
+++ routes/utilitybills.routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const UtilityBill = require('../controller/utilitybills.controller')
-const auth =  require('../controller/auth.controller')
+const auth = require('../controller/auth.controller')
 
 router.post('/', UtilityBill.createUtilityBills)
+router.patch('/', auth.validJWTNeeded, UtilityBill.getAllUtilityByRoom)
 router.patch('/:Id', UtilityBill.update)
-router.get('/', auth.validJWTNeeded, UtilityBill.getAllUtilityByRoom)
-router.get('/:Id',UtilityBill.getById)
+router.get('/:Id', UtilityBill.getById)
 router.delete('/:Id', UtilityBill.delete)
 
 module.exports = router
