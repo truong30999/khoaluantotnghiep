dedb5c6202ecefa01ec004117e00f0f6a15cd21b
truong99 <truongnguyen30999@gmail.com>
2021-10-24 10:30:55 +0700

    fix

--- routes/bill.routes.js
+++ routes/bill.routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const bill = require('../controller/bill.controller')
-const auth = require('../controller/auth.controller')
 
 
 //router.get('/currentmonth',bill.getBillInMonthOfUser)
 router.post('/', bill.createBill)
-router.patch('/', auth.validJWTNeeded, bill.getBillInMonthOfUser)
+router.patch('/', bill.getBillInMonthOfUser)
 router.patch('/:billId', bill.updateBill)
 router.patch('/recalculate/:billId', bill.recalculateBill)
 router.delete('/:billId', bill.deleteBill)
