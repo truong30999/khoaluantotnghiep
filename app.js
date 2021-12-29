bd7695fa2830cd73f1ed3aefc71247fe4a107dc3
truong99 <truongnguyen30999@gmail.com>
2021-12-29 09:57:02 +0700

    update

--- app.js
+++ app.js
@@ -158,10 +158,12 @@ io.on("connection", (socket) => {
   //send and get message
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
-    });
+    if (user) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    }
   });
 
   //when disconnect
