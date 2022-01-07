8377e7bd8477e9b0b3d6b6ddd2d25742304ed2f4
truong99 <truongnguyen30999@gmail.com>
2022-01-07 08:57:25 +0700

    deloy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const CronJobs = require("./controller/cronjob.controller")
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://3.15.7.94:3000");
+  res.header("Access-Control-Allow-Origin", "http://ec2-18-219-191-240.us-east-2.compute.amazonaws.com:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE,OPTIONS");
   res.header(
--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const config = {
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || "mysecretkey",
-  backend_domain: process.env.BACKEND_DOMAIN || "http://3.15.7.94:8080",
-  frontend_domain: process.env.BACKEND_DOMAIN || "http://3.15.7.94:3000",
+  backend_domain: process.env.BACKEND_DOMAIN || "http://ec2-18-219-191-240.us-east-2.compute.amazonaws.com:8080",
+  frontend_domain: process.env.BACKEND_DOMAIN || "http://ec2-18-219-191-240.us-east-2.compute.amazonaws.com:3000",
   API_ACCESS_TOKEN: "aBqgq-LZ2rnbcOOOPTnMTOJsmtwAwX4G",
   API_FIREBASE_PUSH_NOTIFI: "AAAAqS6yhNc:APA91bGdeD624OCVndKXswOr8diqUrFLO-Ex8v5FI2qs8U4tw_z9_WxM42FSjg2Q36g__HbVGQSuUpUkklUgrhrKPHdQj0PlSGG5ron1y-cqUN0uLN5GiePtk_6clCCw0tkChK0jEl4a",
   GOOGLE_CLIENT_ID: "917515198013-tf8flvn0gvpq8p5mj57chd3t1n284pod.apps.googleusercontent.com",
