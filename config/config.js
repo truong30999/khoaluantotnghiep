d988baa68d0c1a4dac96ce43a54da1c85371cdea
truong99 <truongnguyen30999@gmail.com>
2022-01-09 13:31:36 +0700

    deloy

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ const config = {
   frontend_domain: process.env.BACKEND_DOMAIN || "http://quanlyphongtro.ml",
   API_ACCESS_TOKEN: "aBqgq-LZ2rnbcOOOPTnMTOJsmtwAwX4G",
   API_FIREBASE_PUSH_NOTIFI: "AAAAqS6yhNc:APA91bGdeD624OCVndKXswOr8diqUrFLO-Ex8v5FI2qs8U4tw_z9_WxM42FSjg2Q36g__HbVGQSuUpUkklUgrhrKPHdQj0PlSGG5ron1y-cqUN0uLN5GiePtk_6clCCw0tkChK0jEl4a",
-  GOOGLE_CLIENT_ID: "917515198013-tf8flvn0gvpq8p5mj57chd3t1n284pod.apps.googleusercontent.com",
-  GOOGLE_CLIENT_SECRET: "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",
+  GOOGLE_CLIENT_ID: "917515198013-n06ssubctrnebvakaulp7ub1c4mi4opt.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "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",
   MOMO: {
     partnerCode: "MOMO8SGC20220101",
     public_key: "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAhO8nHBkbIDq4PzVd7jR7BJg0nz+6zVz43nRkvKeRMJf/jkMNucHxVekDA89IZjKltznrMdSNeENx5X0J3f/YgE5ktck32uWKzkIrsR3eacG24kBvZ+PGWocpJ+9Elbih3XGUdjvBL6kXRENwQzW+j1gpAlxaoHhw1sKZDqMGzc9Wye1o/Ih04gjekRlhydPaINp37L5A3Pad/Du/OcR27AZxNKQZAhmtRFIDOE5h3oryIjZSIeRxpM6CZl9EBiE8R/BjRT4STuQJNug+Lh4sGOiBYqoqrIlnxoGRutbSVJzO2QVnyL8wb2ygZGkoos3z36g9dfCdM2RQjym4okSpXx3iRPqr1JnCNv1kn2sUrWGwA+xtTF0igHgfT1yYrYRacxDngWjIT2O8BxFpGfl8XgKW9n7G6roEqpgb10ZtbN5XWxAQ3u6mw3Op/5eBtata51PL1+/O6LWDn9i7XXtvU3ORMQIHthhptdm+eZyzE1AdrNLrD3lwJgIoT4Sg58SCsdBVxoAVuH3hdQmrIHOO3/XIlaw0G+xErTheSoHyfDZ8NJRFRihHLu4RixgU60Q0leg9v/39fDZnaxjnNAm1mXIljV+rQ6jPGN5Jb5bun3Ybatgi5uTQfh7MHGfvu0sBH0p3kLLs9CaFoZlqXQVbrh4psBAmsN+Aae1sZhVvuC8CAwEAAQ==",
