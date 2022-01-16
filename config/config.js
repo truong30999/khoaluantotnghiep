eac688d3762f16c9c61bb3a5695a9042d98eca20
truong99 <truongnguyen30999@gmail.com>
2022-01-16 22:46:17 +0700

    change ip

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || "mysecretkey",
-  backend_domain: process.env.BACKEND_DOMAIN || "http://3.84.7.130:8080",
+  backend_domain: process.env.BACKEND_DOMAIN || "http://3.95.159.254:8080",
   frontend_domain: process.env.BACKEND_DOMAIN || "http://quanlyphongtro.ml",
   API_ACCESS_TOKEN: "aBqgq-LZ2rnbcOOOPTnMTOJsmtwAwX4G",
   API_FIREBASE_PUSH_NOTIFI: "AAAAqS6yhNc:APA91bGdeD624OCVndKXswOr8diqUrFLO-Ex8v5FI2qs8U4tw_z9_WxM42FSjg2Q36g__HbVGQSuUpUkklUgrhrKPHdQj0PlSGG5ron1y-cqUN0uLN5GiePtk_6clCCw0tkChK0jEl4a",
